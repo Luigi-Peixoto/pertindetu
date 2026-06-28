712b4605fccb8f7bffd3ccea668d94827c28c1af
Luigi Peixoto <luigisp08@gmail.com>
2026-06-27 23:51:30 -0300

    fix: lint

--- backend/src/main/java/com/ufrn/pertindetu/auth/controller/AuthController.java
+++ backend/src/main/java/com/ufrn/pertindetu/auth/controller/AuthController.java
@@ -5,7 +5,10 @@ import com.ufrn.pertindetu.auth.dto.TokenDTO;
 import com.ufrn.pertindetu.auth.service.AuthService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/v1/auth")
